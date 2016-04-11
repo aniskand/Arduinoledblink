58b48fb8e929c7ac88eb9dcf3fc8c18c97897708
Andrew Iskander <shrikeg@gmail.com>
2016-04-11 15:13:22 -0600

    adding arduino sketch

---
+++ ESP8266_cloud.ino
@@ -0,0 +1,95 @@
+
+/*
+  This a simple example of the aREST Library for the ESP8266 WiFi chip.
+  This example illustrate the cloud part of aREST that makes the board accessible from anywhere
+  See the README file for more details.
+
+  Written in 2015 by Marco Schwartz under a GPL license.
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <aREST.h>
+
+// Clients
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Create aREST instance
+aREST rest = aREST(client);
+
+// Unique ID to identify the device for cloud.arest.io
+char* device_id = "bbquito";
+
+// WiFi parameters
+const char* ssid = "youcantseeme";
+const char* password = "KDS-r60XBR2";
+
+// Variables to be exposed to the API
+int temperature;
+int humidity;
+
+String incomingByte; 
+
+// Functions
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  // Set callback
+  client.setCallback(callback);
+
+  // Init variables and expose them to REST API
+  temperature = 24;
+  humidity = 40;
+  incomingByte = "";
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+  rest.variable("incomingByte",&incomingByte);
+
+  // Give name and ID to device
+  rest.set_id(device_id);
+  rest.set_name("esp8266");
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set output topic
+  char* out_topic = rest.get_topic();
+
+}
+
+void loop() {
+
+  // Connect to the cloud
+  rest.handle(client);
+
+  
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.readString();
+
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte);
+        }
+
+}
+
+// Handles message arrived on subscribed topic(s)
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  rest.handle_callback(client, topic, payload, length);
+
+}
